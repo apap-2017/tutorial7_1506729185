08af67ee290b8b3bfe5c8b0d7b9cf7aed6bd567d
ammar.robbani <ammar.robbani@yahoo.com>
2017-11-01 10:53:50 +0700

    producer dikit lagi bosq

---
+++ Tutorial07/Tutorial07Producer/src/main/java/com/example/rest/CourseRestController.java
@@ -0,0 +1,35 @@
+package com.example.rest;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.model.CourseModel;
+import com.example.service.CourseService;
+
+@RestController
+@RequestMapping("/rest")
+public class CourseRestController {
+	@Autowired
+	CourseService courseService;
+	
+	@RequestMapping("/course/view/{id_course}")
+    public CourseModel view (Model model,
+            @PathVariable(value = "id_course") String id_course)
+    {
+        CourseModel course = courseService.selectCourse (id_course);
+
+        return course;
+    }
+	
+	@RequestMapping("student/viewall")
+	public List<CourseModel> viewall (Model model) {
+		List<CourseModel> courses = courseService.selectCourseStudent();
+
+        return courses;
+	}
+}
---
+++ Tutorial07/Tutorial07Producer/src/main/java/com/example/service/CourseService.java
@@ -0,0 +1,12 @@
+package com.example.service;
+
+import java.util.List;
+
+import com.example.model.CourseModel;
+import com.example.model.StudentModel;
+
+public interface CourseService {
+	CourseModel selectCourse(String id_course);
+	
+	List<StudentModel> selectCourseStudent(String id_course);
+}
